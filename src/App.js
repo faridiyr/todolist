50d1b3a3d2caddf0c7dc0fab9cd88206be29a658
faridiyr <faridiyr@gmail.com>
2022-06-24 00:20:27 +0700

    update css

--- src/App.js
+++ src/App.js
@@ -5,9 +5,9 @@ import TodoList from './component/TodoList';
 function App() {
   return (
     <div className="App">
-      {/* <header className="App-header"> */}
-      <TodoList />
-      {/* </header> */}
+      <header className="App-header">
+        <TodoList />
+      </header>
     </div>
   );
 }
