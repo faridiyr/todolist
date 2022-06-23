864e841b7825c492489f0f8376118d4990b34ee9
faridiyr <faridiyr@gmail.com>
2022-06-23 23:27:18 +0700

    Todo List

--- src/component/TodoList.js
+++ src/component/TodoList.js
@@ -4,13 +4,23 @@ import TodoForm from './TodoForm'
 function TodoList() {
     const [todos, setTodos] = useState([]);
 
-    const
+    const addTodo = todo => {
+        if (!todo.text || /^\s*$/.test(todo.text)) {
+            return
+        }
+
+        const newTodos = [todo, ...todos];
+
+        setTodos(newTodos);
+        console.log(todo, ...todos);
+    }
+
 
 
     return (
         <div>
             <h1>Whats the plan for Today?</h1>
-            <TodoForm />
+            <TodoForm onSubmit={addTodo} />
         </div>
     )
 }
