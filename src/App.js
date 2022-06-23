3758951c46c8334e75653d8b11081723ed8a4756
faridiyr <faridiyr@gmail.com>
2022-06-23 23:14:46 +0700

    Todo Form

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import TodoForm from './component/TodoForm';
+import TodoList from './component/TodoList';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>To Do App</h1>
+        <TodoList />
       </header>
     </div>
   );
---
+++ src/component/TodoForm.js
@@ -0,0 +1,36 @@
+import React, { useState } from 'react'
+
+function TodoForm(props) {
+    const [input, setInput] = useState('');
+
+    const handleChange = event => {
+        setInput(event.target.value);
+    }
+
+    const handleSubmit = event => {
+        event.preventDefault();
+
+        // props.onSubmit({
+        //     id: Math.floor(Math.random() * 10000),
+        //     text: input
+        // })
+
+        setInput('');
+    };
+
+    return (
+        <form className='todo-form'>
+            <input
+                type='text'
+                placeholder='Create To Do'
+                value={input}
+                name='text'
+                className='todo-input'
+                onChange={handleChange}
+            />
+            <button className='todo-button'>Create</button>
+        </form>
+    )
+}
+
+export default TodoForm
